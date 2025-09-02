4c715e31a24341537aa0d992bd62a456d5e61543
Ksenia Erofeeva <ksenchik.bogdanova@gmail.com>
2025-09-02 22:20:23 +0300

    Исправлен файл test_registration.py: открытие и закрытие файла сделаны в фикстуре, использованы явные ожидания selenium`а

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -3,25 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from locators.locators import RegistrationLocators
 from helpers.generators import generate_unique_email  # Импортируем функцию из generators.py
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def test_successful_registration():
-    driver = webdriver.Chrome()
+def test_successful_registration(driver):
+    wait = WebDriverWait(driver, 10)
+
+    # Открываем страницу и ждем загрузки формы
     driver.get("https://stellarburgers.nomoreparties.site/register")
+    wait.until(EC.visibility_of_element_located(RegistrationLocators.NAME_INPUT))
 
-    # Вводим имя
+    # Заполняем форму
     driver.find_element(*RegistrationLocators.NAME_INPUT).send_keys("Тестовый")
 
-    # Обновленный локатор для поля Email
-    email_input = driver.find_element(By.XPATH, "//label[text()='Email']/..//input")
-    email_input.send_keys(generate_unique_email())  # Используем функцию для генерации уникального email
+    email_input = wait.until(EC.visibility_of_element_located((By.XPATH, "//label[text()='Email']/..//input")))
+    email_input.send_keys(generate_unique_email())
 
-    # Вводим пароль
-    driver.find_element(*RegistrationLocators.PASSWORD_INPUT).send_keys("TestPassword123")
+    driver.find_element(*RegistrationLocators.PASSWORD_INPUT).send_keys("TestPassword123!")
 
-
-    # Кликаем по кнопке регистрации
+    # Кликаем и ждем перенаправления
     driver.find_element(*RegistrationLocators.REGISTER_BUTTON).click()
 
-
-    # Закрытие браузера
-    driver.quit()
+    # Ждем перенаправления на страницу логина
+    wait.until(EC.url_to_be("https://stellarburgers.nomoreparties.site/login"))
+    assert "login" in driver.current_url
